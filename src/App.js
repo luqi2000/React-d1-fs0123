1ed5578dc61cfc61c7286a1803bfce227c0d7455
luqi2000 <luqmanmohammad09@gmail.com>
2023-03-27 13:55:20 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ButtonComponent from "./components/ButtonComponent";
+import ImageComponent from "./components/ImageComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <ButtonComponent text="Prima immagine" />
+        <ImageComponent
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ__7e_BarZ2W8YkIGpRg5lUvWqmaR5xvpcw&usqp=CAU"
+          alt="Immagine"
+        />
+
+        <ButtonComponent text="Seconda immagine" />
+        <ImageComponent
+          src="https://png.pngitem.com/pimgs/s/3-36435_small-mario-super-mario-moving-hd-png-download.png"
+          alt="Immagine"
+        />
       </header>
     </div>
   );
---
+++ src/components/ButtonComponent.jsx
@@ -0,0 +1,4 @@
+function ButtonComponent(props) {
+  return <button>{props.text}</button>;
+}
+export default ButtonComponent;
