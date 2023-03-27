e69d99855c14988ad281b4be12da295f707fa650
luqi2000 <luqmanmohammad09@gmail.com>
2023-03-27 17:33:27 +0100

    agg

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 class ImageComponent extends Component {
   render() {
     return <img src={this.props.src} alt={this.props.alt} />;
